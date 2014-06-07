f2fc8a6e39ea57733d3eb85007b8d04ecc62e46c
Pat Cavit <pcavit@gmail.com>
2014-06-06 23:38:44 -0700

    Tokenizer improvements

--- tasks/filter-items.js
+++ tasks/filter-items.js
@@ -7,8 +7,8 @@ var Tokenizer = require("../lib/tokenizer");
 module.exports = function(config, done) {
     var t = new Tokenizer(true);
     
-    t.rule("test", /a/);
-    t.rule("test", /b/);
+    t.rule("a", /a/);
+    t.rule("b", /b/);
     
     t.tokenize("bbba", function(error, tokens) {
         console.log(arguments);
