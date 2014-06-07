b0175aaab5d0bdc9f7c1ab477dd30fb2b5d5df83
Pat Cavit <pcavit@gmail.com>
2014-06-07 16:43:56 -0700

    Filter items defining scene tokens

--- tasks/filter-items.js
+++ tasks/filter-items.js
@@ -2,18 +2,54 @@
 
 "use strict";
 
-var Tokenizer = require("../lib/tokenizer");
+var async     = require("async"),
+    xregexp   = require("xregexp").XRegExp,
+    Tokenizer = require("../lib/tokenizer"),
+    scene     = new Tokenizer(),
+    extract   = /^File: (.*) Thread:/;
+
+scene.rule("extension", xregexp("\\.(?<extension> mp4|mkv|avi)", "i"));
+scene.rule("quality",   xregexp("720p|1080p|1080i", "i"));
+scene.rule("source",    xregexp("HDTV|webrip|bluray|web-dl", "i"));
+scene.rule("encoder",   xregexp("x264|xvid|H.264|AAC2\\.0", "i"));
+scene.rule("air-date",  xregexp("(?<year>\\d{4})[. _-]+(<?month>\\d{2})(<?day>\\d{2})", "i"));
+scene.rule("season-ep", xregexp("s(?<season>\\d+)e(?<episode>\\d+)", "i"));
+scene.rule("season-ep", xregexp("(?<season>\\d+)x(?<episode>\\d+)", "i"));
+scene.rule("group",     xregexp("-(<?group>\\w+)", "i"));
+scene.rule("seperator", xregexp("[. _-]+", "i"));
+scene.rule("title",     xregexp("\\w+", "i"));
 
 module.exports = function(config, done) {
-    var t = new Tokenizer(true);
-    
-    t.rule("a", /a/);
-    t.rule("b", /b/);
-    
-    t.tokenize("bbba", function(error, tokens) {
-        console.log(arguments);
+    async.map(
+        config.entries,
+        function(entry, cb) {
+            var title = entry.title.trim();
         
-        done();
-    });
+            if(title.search(extract) > -1) {
+                title = title.match(extract)[1];
+            }
+            
+            console.log(title);
+            
+            scene.tokenize(title, function(error, tokens) {
+                if(error) {
+                    config.log("warn", "Unable to parse " + title);
+                }
+                
+                console.log(tokens);
+                
+                cb(null, error ? title : tokens);
+            });
+        },
+        function(error, results) {
+            if(error) {
+                return done(error);
+            }
+            
+            config.parsed = results;
+            
+            done();
+        }
+    );
 };
 
