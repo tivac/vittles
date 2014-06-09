52162c2d9e2b1dc31b90ad39acd8d16e510e3984
Pat Cavit <pcavit@gmail.com>
2014-06-08 22:24:25 -0700

    Tokenizer doesn't take callback
    Moved scene name parsing from task into lib file

--- tasks/filter-entries.js
+++ tasks/filter-entries.js
@@ -2,40 +2,42 @@
 
 "use strict";
 
-var async    = require("async"),
-    FuzzySet = require("fuzzyset.js");
+var FuzzySet = require("fuzzyset.js");
 
-module.exports = function(config, done) {
+module.exports = function(config) {
     var fuzzyset = new FuzzySet(
             config.parsed.map(function(item) {
                 return item.title;
             })
-        );
+        ),
+        shows = {};
     
-    async.each(
-        config.shows,
-        function(show, cb) {
-            var results = fuzzyset.get(show),
-                title, entries;
+    config.shows.forEach(function(show) {
+        var results = fuzzyset.get(show),
+            title;
         
-            if(!results) {
-                config.log("warn", "Couldn't find a match for " + show);
-                
-                return cb();
-            }
+        if(!results) {
+            config.log("warn", "Couldn't find a match for " + show);
             
-            results.sort(function(a, b) {
-                return a[0] > b[0];
-            });
-            
-            title = results[0][1];
-            
-            entries = config.parsed.filter(function(entry) {
-                return entry.title === title;
-            });
-            
-            console.log(entries);
-        },
-        done
-    );
+            return;
+        }
+        
+        results.sort(function(a, b) {
+            return a[0] > b[0];
+        });
+        
+        config.log("silly", "Matching results: %j", results);
+        
+        if(results[0][0] < 0.5) {
+            return;
+        }
+        
+        title = results[0][1];
+        
+        shows[title] = config.parsed.filter(function(entry) {
+            return entry.title === title;
+        });
+    });
+    
+    config.choices = shows;
 };
