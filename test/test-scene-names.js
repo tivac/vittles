16c1cf3aa6d0c83fa6f19a455bb1800bc53fd1bd
Pat Cavit <pcavit@gmail.com>
2014-06-23 22:39:53 -0700

    remove errant debugger

--- test/test-scene-names.js
+++ test/test-scene-names.js
@@ -8,7 +8,6 @@ describe("Scene Name Parser", function() {
 
     Object.keys(names).forEach(function(name) {
         it("should parse \"" + name + "\" correctly", function() {
-            debugger;
             var details = parse(name);
 
             assert.deepEqual(details, names[name].details);
